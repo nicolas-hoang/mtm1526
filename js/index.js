6f7ea5f7d573c7d4d22837922d759602995b379c
Chuong Hoang <167705389+nicolas-hoang@users.noreply.github.com>
2024-05-16 13:10:06 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,44 @@
+// TASK 1
+let a = 3;
+let b = 5;
+let c;
+
+let task1Output =`
+    let a = 3;
+    let b = 5;
+    let c;
+    ----------
+    a + b = ${a+b}
+    a - b = ${a-b}
+    a * b = ${a*b}
+    a / b = ${a/b}
+    a % b = ${a%b}
+    a += b = ${a+=b}
+    a -= b = ${a-=b}
+    a *= b = ${a*=b}
+    a /= b = ${a/=b}
+    a %= b = ${a%=b}
+    a == b = ${a==b}
+    a != b = ${a!=b}
+    a > b = ${a>b}
+    a < b = ${a<b}
+    !a && !b = ${!a && !c}
+    !a || !b = ${!a || !c}`;
+
+console.log(task1Output);
+
+
+// TASK 2
+// Declare the variable first_name and assign it with the value of your first name:
+let first_name = 'Chuong';
+
+// Declare the variable last_name and assign it with the value of your last name:
+let last_name = 'Hoang';
+
+// Declare the variable email and assign it with the value of your email:
+let email = 'khan0560@algonquinlive.com';
+
+// Create the expression concatenating variables first_name, last_name, email and string:
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+console.log(output);
